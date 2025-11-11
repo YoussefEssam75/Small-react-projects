16986d5e6da01f010cc873e40999e37ff7e1e088
Youssef Essam Yehia Ghareeb <137729218+YoussefEssam75@users.noreply.github.com>
2025-11-11 16:40:33 +0200

    update

--- travil-list/src/App.js
+++ travil-list/src/App.js
@@ -1,16 +1,14 @@
 import { useState } from "react";
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handeleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handeleAddItem} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,13 +18,15 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form(onAddItems) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
     const newitem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newitem);
     setDescription("");
     setQuantity(1);
   }
@@ -54,11 +54,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
