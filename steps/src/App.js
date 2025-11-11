c4af1cce16bed4678613a393928401d9684a330c
Youssef Essam Yehia Ghareeb <137729218+YoussefEssam75@users.noreply.github.com>
2025-11-11 13:41:27 +0200

    update

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,4 +1,21 @@
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "lightblue" }} className="buttons">
+          Previous
+        </button>
+        <button style={{ backgroundColor: "lightblue" }} className="buttons">
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 export default App;
