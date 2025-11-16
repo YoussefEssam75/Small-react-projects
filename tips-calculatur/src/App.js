69c0264a1687669c1b0ab3014470ff97ab1239d7
Youssef Essam Yehia Ghareeb <137729218+YoussefEssam75@users.noreply.github.com>
2025-11-16 13:23:35 +0200

    update

--- tips-calculatur/src/App.js
+++ tips-calculatur/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
 
-function App() {
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <TipCalculatur/>
+      </div>
   );
 }
 
-export default App;
+function TipCalculatur(){
+  const [billamount, setBillamount] = useState(0);
+  const[persntage1 , setpersntage1] = useState(0);
+  const[persntage2 , setpersntage2] = useState(0);
+const tip =billamount * (persntage1 + persntage2) /200;
+return <div>
+  <BillInput billamount={billamount} onsetBillamount={setBillamount}/>
+  <TipSelector persntage1={persntage1} onselect={setpersntage1}> please rate the service used </TipSelector>
+  <TipSelector persntage2={persntage2} onselect={setpersntage2}> please make your friend rate the service you used </TipSelector>
+  <Output billamount={billamount} tip={tip}/>
+  <Reset onReset={() => {setBillamount(""); setpersntage1(0); setpersntage2(0);}}/>
+</div>
+}
+
+function BillInput({billamount, onsetBillamount}){
+return <div>
+  <label> How much was the bill ? </label>
+  <input type="text" placeholder="Enter Amount" value={billamount} onChange={(e)=> onsetBillamount(Number(e.target.value))}/>
+</div>
+}
+
+function TipSelector({children , persntage , onselect}){
+  
+return <div>  
+  <label>{children}</label>
+  <select value={persntage} onChange={(e)=> onselect(Number(e.target.value))}> 
+    <option value="0"> So Bad </option>
+    <option value="5"> Not Bad </option>
+    <option value="10"> Good </option>
+    <option value="15"> So Good </option>
+    <option value="20"> Fantastic </option> 
+  </select>
+</div>
+}
+
+function Output({billamount, tip}  ){
+return <div>
+  <h3> Bill Amount : {billamount} $ </h3>
+  <h3> Tip Amount : {tip} $ </h3>
+  <h3> Total Amount : {billamount + tip} $ </h3>
+</div>
+}
+
+function Reset({onReset}){
+    return <div>
+      <button onClick={onReset}> Reset </button>
+    </div>
+}
