68a14d1d838c9482a58aba5738f3ed034a11edd0
Youssef Essam Yehia Ghareeb <137729218+YoussefEssam75@users.noreply.github.com>
2025-11-16 14:03:21 +0200

    update

--- eat-and-split/src/App.js
+++ eat-and-split/src/App.js
@@ -1,5 +1,3 @@
-
-import './App.css';
 const initialFriends = [
   {
     id: 118836,
@@ -22,12 +20,41 @@ const initialFriends = [
 ];
 
 
-function App() {
-  return (
-    <div className="App">
+export default function App() {
 
-    </div>
-  );
+return (
+      <div className="app">
+        <div className="sidebar"> 
+          <FriendsList/>
+        </div>
+      </div>
+    );
 }
 
-export default App;
+
+function FriendsList() {
+  return (
+    <ul>
+      {initialFriends.map((friend) => (
+        <li key={friend.id}>
+          <img
+            src={friend.image}
+            alt={friend.name}
+          />
+          <h3>{friend.name}</h3>
+          {friend.balance < 0 && (
+            <p className="red">
+              You owe {friend.name} ${Math.abs(friend.balance)}
+            </p>
+          )}
+          {friend.balance > 0 && (
+            <p className="green">
+              {friend.name} owes you ${friend.balance}
+            </p>
+          )}
+          {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+        </li>
+      ))}
+    </ul>
+  );
+}
